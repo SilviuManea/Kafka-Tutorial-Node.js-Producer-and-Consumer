cea7bb739509d3a5e14052b65bb3d0c2fc0e4c3e
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-11-16 13:32:21 +0100

    4-Consumer logic for simple strings working OK

--- consumer/index.js
+++ consumer/index.js
@@ -1,1 +1,24 @@
 console.log('consumer...');
+
+import Kafka from 'node-rdkafka';
+
+//Create the consumer
+const consumer = Kafka.KafkaConsumer({
+    'group.id':'kafka',
+    //We define our broker
+    'metadata.broker.list' : 'localhost:9092' }, {});
+
+//connect the consumer
+consumer.connect();
+
+//when consumer is ready
+consumer.on('ready',()=>{
+    console.log('consumer ready...')
+    //subscribe to one topic(or more than one.)
+    consumer.subscribe(['test']);
+    //consume the events for that topic as they flow in
+    consumer.consume();
+}).on('data',(data) => {
+    //any time any new data comes in this function is going to be called.
+    console.log(`received message: ${data.value}`);
+});
