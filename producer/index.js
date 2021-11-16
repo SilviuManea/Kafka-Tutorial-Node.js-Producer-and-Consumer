adfdd67636a92310fd18ed44d2edc83e075c0210
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-11-16 17:28:18 +0100

    5-Preparing random content for the objects working ok

--- producer/index.js
+++ producer/index.js
@@ -8,11 +8,28 @@ const stream = Kafka.Producer.createWriteStream({
     //We define our broker and topic
     'metadata.broker.list' : 'localhost:9092' }, {} , {topic:'test'});
 
+function getRandomAnimal(){
+    const categories = ['CAT','DOG']; //THIS values are the ones available on the eventType enum. The process will not work if we pass some other value that is nos declared there.
+    return categories[Math.floor(Math.random() * categories.length)]; //returns a random index
+}
+
+function getRandomNoise(animal){
+    if(animal === 'CAT'){
+        const noises = ['purr','meow'];
+        return noises[Math.floor(Math.random() * noises.length)]; //returns a random index
+    }else if (animal === 'DOG'){
+        const noises = ['woof','bark'] //THIS values are the ones available on the eventType enum. The process will not work if we pass some other value that is nos declared there.
+        return noises[Math.floor(Math.random() * noises.length)]; //returns a random index
+    }
+ 
+}
+
 //Writing objects to the stream
 function queueMessage(){
-
+        const category = getRandomAnimal();
+        const noise = getRandomNoise(category);
         //We define the event(object in our case)
-        const event = {category: 'DOG', noise: 'bark'};
+        const event = {category, noise};
         //We write the event to the buffer using eventType.toBuffer(event)
         const success = stream.write(eventType.toBuffer(event));
         if(success){
