a3754c7aa7348f671d649009b4ea204d913112b8
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-11-16 13:17:34 +0100

    4-Producer-Publishing events to the Kafka topic

--- producer/index.js
+++ producer/index.js
@@ -1,1 +1,23 @@
 console.log('producer...');
+
+import Kafka from 'node-rdkafka';
+
+//Create a stream to which we write our events to
+const stream = Kafka.Producer.createWriteStream({
+    //We define our broker and topic
+    'metadata.broker.list' : 'localhost:9092' }, {} , {topic:'test'});
+
+function queueMessage(){
+    const success = stream.write(Buffer.from('hola'));
+    if(success){
+        console.log('message wrote successfully to stream')
+    }else{
+        console.log('something went wrong...');
+    }
+    
+}
+
+//Set interval to write messages or events to the stream(for example each 3 seconds)
+setInterval(() =>{
+    queueMessage();
+},3000)
